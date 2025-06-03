efd7cfe198b9d5c7fe7d226fc8b84b10826c5abe
Marco Teodoro Mancuso <marco.teo.mancuso@gmail.com>
2025-06-03 17:51:15 +0200

    stampa degli oridni aggiunta

--- src/main/java/it/epicode/u5w1d1teoria/Runner.java
+++ src/main/java/it/epicode/u5w1d1teoria/Runner.java
@@ -15,5 +15,29 @@ public class Runner implements CommandLineRunner {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
         Menu menu = new Menu(ctx);
         menu.stampaMenu();
+
+        Ordine ordine1 = (Ordine) ctx.getBean("ordine1");
+        Ordine ordine2 = (Ordine) ctx.getBean("ordine2");
+        Ordine ordine3 = (Ordine) ctx.getBean("ordine3");
+
+        stampaOrdine(ordine1);
+        stampaOrdine(ordine2);
+        stampaOrdine(ordine3);
+
+        ctx.close();
+    }
+
+    private void stampaOrdine(Ordine ordine) {
+        System.out.println("\n===============================");
+        System.out.println("ORDINE N°: " + ordine.getNumeroOrdine());
+        System.out.println("Tavolo: " + ordine.getTavolo().getNumeroTavolo());
+        System.out.println("Coperti: " + ordine.getNumeroCoperti());
+        System.out.println("Stato: " + ordine.getStatoOrdine());
+        System.out.println("Ora: " + ordine.getOraAcquisizione());
+        System.out.println("Prodotti:");
+        ordine.getProdotti().forEach(p -> {
+            System.out.println(" - " + p.getNome() + " | €" + p.getPrezzo() + " | " + p.getCalorie() + " cal");
+        });
+        System.out.printf("Totale ordine (con coperto): €%.2f%n", ordine.getTotale());
     }
 }
